8178eed54ae4aae8bdf8bddbc05a8eb67ae296e0
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-03-20 10:15:00 +0530

    Day 20 - Add routing for Lesson20

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Lesson15 from "./pages/Lesson15";
 import Lesson16 from "./pages/Lesson16";
 import Lesson17 from "./pages/Lesson17";
 import Lesson18 from "./pages/Lesson18";
+import Lesson19 from "./pages/Lesson19";
 // Import all other lessons similarly...
 
 
