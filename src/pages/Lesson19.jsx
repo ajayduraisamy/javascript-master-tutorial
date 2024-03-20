877d3efafe1d5280f40b477d13092d2172cd5f71
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-03-20 18:45:00 +0530

    Day 20 - Final fixes and cleanup for Lesson20

---
+++ src/pages/Lesson19.jsx
@@ -0,0 +1,7 @@
+git add.
+
+    $env: GIT_AUTHOR_DATE = "2024-03-20T13:00:00"
+$env: GIT_COMMITTER_DATE = "2024-03-20T13:00:00"
+
+git commit - m "Day 20 - Improve Design Patterns content and UI"
+git push
