6f451b27bc62caa0af4a9f75e2f7b3ed9b20aa9c
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-03-20 11:40:00 +0530

    Day 20 - Add Lesson20 to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ export default function App() {
         <Route path="/lesson16" element={<Lesson16 />} />
         <Route path="/lesson17" element={<Lesson17 />} />
         <Route path="/lesson18" element={<Lesson18 />} />
+        <Route path="/lesson19" element={<Lesson19 />} />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
